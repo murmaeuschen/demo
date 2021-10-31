b6dc77869feb59424c6f1edcc658c9bbb46efe68
ChumakO <chumako@gmail.com>
2021-10-31 19:29:18 +0100

    add gitignore

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -4,12 +4,15 @@ import com.example.demo.model.EnvSystemConfig;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
 public class DemoController {
 
     @GetMapping(value="/check")
     public EnvSystemConfig getCheckConfig(){
-        EnvSystemConfig env = new EnvSystemConfig(123L);
+        EnvSystemConfig env = new EnvSystemConfig(123L, new Date());
+
         return env;
     }
 
--- src/main/java/com/example/demo/model/EnvSystemConfig.java
+++ src/main/java/com/example/demo/model/EnvSystemConfig.java
@@ -2,10 +2,15 @@ package com.example.demo.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class EnvSystemConfig {
 
     public Long emloyeeNum;
+    public Date date;
 }
